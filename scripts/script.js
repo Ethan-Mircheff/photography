a01fd660761ab1bc86a2fbb9af4be4bdd5cd095c
Ethan Mircheff <163207625+Ethan-Mircheff@users.noreply.github.com>
2025-03-10 12:04:11 -0700

    Update classes names

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     $(".imgSmall").click(function () {
         $("#imgBig").attr("src", $(this).prop('src'));
-        $("#hoverOverlay").show();
-        $("#hoverOverlayContent").show();
+        $("#clickOverlay").show();
+        $("#clickOverlayContent").show();
     });
 
-    $("#imgBig, #hoverOverlay").click(function () {
+    $("#imgBig, #clickOverlay").click(function () {
         $("#imgBig").attr("src", "");
-        $("#hoverOverlay").hide();
-        $("#hoverOverlayContent").hide();
+        $("#clickOverlay").hide();
+        $("#clickOverlayContent").hide();
     });
 });
