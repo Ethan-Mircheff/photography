5831ef6b3d8737d148bdc7dd60860dd439af58ad
Ethan Mircheff <163207625+Ethan-Mircheff@users.noreply.github.com>
2026-03-30 14:39:38 -0700

    Added missing quotation marks

--- scripts/script.js
+++ scripts/script.js
@@ -19,15 +19,16 @@ var onClick = function (element, handler) {
 };
 
 ready(function () {
-  onClick(.pic, function () {
+  onClick(".pic", function () {
       $("#imgBig").attr("src", $(this).prop('src'));
       $("#clickOverlay").fadeIn(100);
       $("#clickOverlayContent").fadeIn(100);
   });
-  onClick(#imgBig, #clickOverlay, function () {
+  onClick("#imgBig, #clickOverlay", function () {
       $("#imgBig").attr("src", "");
       $("#clickOverlay").fadeOut(100);
       $("#clickOverlayContent").fadeOut(100);
     });
   });
 });
+
