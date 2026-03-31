089eca45e75491a8c91aade37ab51b305953e51e
Ethan Mircheff <163207625+Ethan-Mircheff@users.noreply.github.com>
2026-03-31 13:18:01 -0700

    Temporarily revert to using jQuery to fix issues

--- scripts/script.js
+++ scripts/script.js
@@ -21,30 +21,11 @@ var onClick = function (element, handler) {
 ready(function () {
   document.querySelectorAll(".pic").forEach(function (element) {
     onClick(element, function () {
-      document.querySelector("#imgBig").setAttribute("src", this.src);
-      function fadeIn(el, speed = 100) {
-        var opacity = 0;
-        el.style.opacity = 0;
-        el.style.filter = "";
-        var last = +new Date();
-        var tick = function () {
-          opacity += (new Date() - last) / speed;
-          if (opacity > 1) opacity = 1;
-          el.style.opacity = opacity;
-          el.style.filter = "alpha(opacity=" + (100 * opacity || 0) + ")";
-          last = +new Date();
-          if (opacity < 1) {
-            (window.requestAnimationFrame && requestAnimationFrame(tick)) ||
-              setTimeout(tick, 16);
-          }
-        };
-        tick();
-      }
-      fadeIn("#clickOverlay");
-      fadeIn("#clickOverlayContent");
+    document.querySelector("#imgBig").setAttribute("src", this.src);
+    $("#clickOverlay").fadeIn(100);
+    $("#clickOverlayContent").fadeIn(100);
     });
   });
-  
   onClick(document.querySelector("#imgBig"), function () {
     document.querySelector("#imgBig").setAttribute("src", "");
     $("#clickOverlay").fadeOut(100);
