40bfc9c9d7a9ad33a8d78c0a894882889ed1c0fd
Ethan Mircheff <163207625+Ethan-Mircheff@users.noreply.github.com>
2026-04-23 10:26:22 -0700

    Replaced jQuery for fadeIn & fadeOut with native JavaScript to remove external dependency.

--- scripts/script.js
+++ scripts/script.js
@@ -18,22 +18,70 @@ var onClick = function (element, handler) {
   }
 };
 
+function fadeIn(el, speed = 400) {
+  var opacity = 0;
+
+  el.style.opacity = 0;
+  el.style.filter = '';
+
+  var last = +new Date();
+  var tick = function () {
+    opacity += (new Date() - last) / speed;
+    if (opacity > 1) opacity = 1;
+    el.style.opacity = opacity;
+    el.style.filter = 'alpha(opacity=' + (100 * opacity || 0) + ')';
+
+    last = +new Date();
+
+    if (opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) ||
+        setTimeout(tick, 16);
+    }
+  };
+
+  tick();
+}
+
+function fadeOut(el, speed = 400) {
+  var opacity = 1;
+
+  el.style.opacity = 1;
+  el.style.filter = '';
+
+  var last = +new Date();
+  var tick = function () {
+    opacity -= (new Date() - last) / speed;
+    if (opacity < 0) opacity = 0;
+    el.style.opacity = opacity;
+    el.style.filter = 'alpha(opacity=' + (100 * opacity || 0) + ')';
+
+    last = +new Date();
+
+    if (opacity > 0) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) ||
+        setTimeout(tick, 16);
+    }
+  };
+
+  tick();
+}
+
 ready(function () {
   document.querySelectorAll(".pic").forEach(function (element) {
     onClick(element, function () {
-    document.querySelector("#imgBig").setAttribute("src", this.src);
-    $("#clickOverlay").fadeIn(100);
-    $("#clickOverlayContent").fadeIn(100);
+      document.querySelector("#imgBig").setAttribute("src", this.src);
+      fadeIn(document.querySelector("#clickOverlay"), 100);
+      fadeIn(document.querySelector("#clickOverlayContent"), 100);
     });
   });
   onClick(document.querySelector("#imgBig"), function () {
     document.querySelector("#imgBig").setAttribute("src", "");
-    $("#clickOverlay").fadeOut(100);
-    $("#clickOverlayContent").fadeOut(100);
+    fadeOut(document.querySelector("#clickOverlay"), 100);
+    fadeOut(document.querySelector("#clickOverlayContent"), 100);
   });
   onClick(document.querySelector("#clickOverlay"), function () {
     document.querySelector("#imgBig").setAttribute("src", "");
-    $("#clickOverlay").fadeOut(100);
-    $("#clickOverlayContent").fadeOut(100);
+    fadeOut(document.querySelector("#clickOverlay"), 100);
+    fadeOut(document.querySelector("#clickOverlayContent"), 100);
   });
 });
